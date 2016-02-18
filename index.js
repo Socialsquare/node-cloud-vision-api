5a910a996e91c2cf316221c963eb4a9c6c62de8a
Viktor Shamal Andersen <Sphinxominator@gmail.com>
2016-02-18 15:18:24 +0100

    Added a v1 client class

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const __ = require('underscore')
 
 module.exports = {
-  
+
   Request: require('./lib/models/Request'),
 
   Feature: require('./lib/models/Feature'),
@@ -12,7 +12,7 @@ module.exports = {
   _client: null,
 
   init(options) {
-    this._options = __.defaults(options, {version: 'v1alpha1'})
+    this._options = __.defaults(options, {version: 'v1'})
     const Endpoint = require('./lib/clients/' + options.version)
     var ep = new Endpoint(options)
     ep.google = this
@@ -22,7 +22,7 @@ module.exports = {
   annotate(requests) {
     return new Promise((resolve, reject) => {
       if (!requests) { return reject() }
-      if (!__.isArray(requests)) { requests = [requests] }    
+      if (!__.isArray(requests)) { requests = [requests] }
       this._client.annotate(requests).then(resolve, reject)
     })
   }
